53fc659310a555243a82785c1d3d36e732e38d6d
Sergio Daniel Lozano <sergio.lozano@grability.com>
2016-05-26 14:03:47 -0500

    Initial Commit

---
+++ Bankai/Bankai.h
@@ -0,0 +1,19 @@
+//
+//  Bankai.h
+//  Bankai
+//
+//  Created by Sergio Daniel Lozano on 5/26/16.
+//  Copyright © 2016 Sergio Daniel L Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bankai.
+FOUNDATION_EXPORT double BankaiVersionNumber;
+
+//! Project version string for Bankai.
+FOUNDATION_EXPORT const unsigned char BankaiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bankai/PublicHeader.h>
+
+
