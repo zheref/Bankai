74145827d3e92d684665ebda145c9a16e9d4e936
Sergio Daniel <me@zheref.io>
2024-06-13 10:23:41 -0500

    Introduce int seconds counter

---
+++ Kotlin/BankaiReactive/src/main/kotlin/primitives/IntExtensions.kt
@@ -0,0 +1,19 @@
+package io.zheref.bankai.reactive.primitives
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.yield
+
+fun Int.secondsCounter(): Flow<Int> {
+    val upTo = this
+    return flow {
+        for (i in 1..upTo) {
+            emit(i)
+            delay(1000)
+        }
+        yield()
+    }
+}
+
+suspend fun Int.secondsCount() = this.secondsCounter()
