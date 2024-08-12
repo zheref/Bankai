ba7bee06e9b6d9377a83c6ca0d43d96a3889c84d
Sergio Daniel <me@zheref.io>
2024-08-12 01:03:52 -0500

    WIP

--- Kotlin/BankaiCore/src/main/kotlin/parity/Standard.kt
+++ Kotlin/BankaiCore/src/main/kotlin/parity/Standard.kt
@@ -2,9 +2,9 @@ package io.zheref.bankai.core.parity
 
 public typealias Void = Unit
 
-public abstract class Result<T, E: Exception> {}
-public data class SuccessResult<T, E: Exception>(val value: T): Result<T, E>()
-public data class FailureResult<T, E: Exception>(val exception: E): Result<T, E>()
+public abstract class Result<out T, out E: Exception> {}
+public data class SuccessResult<out T, out E: Exception>(val value: T): Result<T, E>()
+public data class FailureResult<out T, out E: Exception>(val exception: E): Result<T, E>()
 
 // Void to void ONCE
 public typealias ZBlock = (Exception?) -> Unit
@@ -16,3 +16,23 @@ public typealias ZFuture<T> = suspend () -> T
 // Void to 1..* values
 public typealias ZYielderOf<T, E> = ((Result<T, E>) -> Void) -> Void
 
+data class ZBinding<T>(
+    val get: () -> T,
+    var set: (T) -> Unit
+) {
+    companion object {
+        fun <T> create(initialValue: T): ZBinding<T> {
+            var value = initialValue
+            return ZBinding(
+                get = { value },
+                set = { newValue -> value = newValue }
+            )
+        }
+
+        fun <T> constant(value: T): ZBinding<T>
+            = ZBinding(
+                get = { value },
+                set = { _ -> }
+            )
+    }
+}
