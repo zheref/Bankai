ecfa6ef7dc190b9e103b6f01d5c977b2a897266e
Sergio Daniel <me@zheref.io>
2024-08-18 16:49:05 -0500

    Check/uncheck tasks operation

--- Kotlin/BankaiCore/src/main/kotlin/utils/Collections.kt
+++ Kotlin/BankaiCore/src/main/kotlin/utils/Collections.kt
@@ -1,7 +1,1 @@
 package io.zheref.bankai.core.utils
-
-public fun <T> mutableListWith(elements: List<T>): MutableList<T> {
-    val mutableList: MutableList<T> = mutableListOf()
-    mutableList.addAll(elements)
-    return mutableList
-}
