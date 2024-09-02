b76c43a71f6b64adf6db0bd678e01c8828e0b65d
Sergio Daniel <me@zheref.io>
2024-09-01 22:46:27 -0500

    WIP: testRegularFetch

---
+++ Swift/BankaiCore/Sources/BankaiCore/Date+Extensions.swift
@@ -0,0 +1,44 @@
+//
+//  Date+Extensions.swift
+//  
+//
+//  Created by Sergio Daniel on 1/09/24.
+//
+
+import Foundation
+
+extension Date {
+    static func fromDateComponents(year: UInt16, month: UInt8, day: UInt8) -> Date? {
+        var components = DateComponents()
+
+        components.year = Int(year)
+        components.month = Int(month)
+        components.day = Int(day)
+
+        return Calendar.current.date(from: components)
+    }
+
+    static func fromTimeComponents(hours: UInt8, minutes: UInt8, seconds: UInt8) -> Date? {
+        var components = DateComponents()
+
+        components.hour = Int(hours)
+        components.minute = Int(minutes)
+        components.second = Int(seconds)
+
+        return Calendar.current.date(from: components)
+    }
+
+    static var endOfDay: Date {
+        var components = Calendar.current.dateComponents([.year, .month, .day], from: Date())
+        components.hour = 23
+        components.minute = 59
+        components.second = 59
+        return Calendar.current.date(from: components)!
+    }
+}
+
+extension DateComponents {
+    var asDate: Date? {
+        Calendar.current.date(from: self)
+    }
+}
--- Swift/BankaiCore/Tests/BankaiCoreTests/Repository/RepositoryTests.swift
+++ Swift/BankaiCore/Tests/BankaiCoreTests/Repository/RepositoryTests.swift
@@ -87,4 +87,76 @@ class RepositoryXCTests: XCTestCase {
         XCTAssertTrue(flow.hasCompleted)
     }
     
+    @MainActor
+    func testRegularFetch() {
+        let baseDate = Date.fromDateComponents(year: 2024, month: 9, day: 1)!
+        let baseDateNextDay = baseDate.addingTimeInterval(86400)
+        
+        var mockedMemorySource = [TestObject]()
+        let mockedLocalSource: [TestObject] = [
+            .init(id: 1, name: "Dog", updatedAt: nil),
+            .init(id: 2, name: "Cat", updatedAt: Date()),
+            .init(id: 3, name: "Bird", updatedAt: nil),
+            .init(id: 4, name: "Fish", updatedAt: baseDate),
+            .init(id: 5, name: "Turtle", updatedAt: nil),
+        ]
+        let mockedRemoteSource: [TestObject] = [
+            .init(id: 1, name: "Dog1", updatedAt: nil),
+            .init(id: 2, name: "Cat1", updatedAt: Date()),
+            .init(id: 3, name: "Bird1", updatedAt: nil),
+            .init(id: 4, name: "Fish1", updatedAt: baseDateNextDay),
+            .init(id: 5, name: "Turtle1", updatedAt: nil),
+        ]
+        var remoteCalled = false
+        
+        // Given
+        let repository: ARepositoryOf<
+            TestObject, TestFilter, FixtureLocal, FixtureRemote
+        > = .init(local: .init(storeFixture: { items in
+            // Do something with these items
+        }, retrieveFixture: { filter in
+            guard let filter else {
+                return mockedLocalSource
+            }
+            
+            return mockedLocalSource.filter {
+                $0.name.precomposedStringWithCanonicalMapping.contains(filter.keywords?.lowercased() ?? "")
+            }
+        }))
+        
+        // We don't set any remote as we intend this case to be local only
+        
+        // Prepare expectations
+        let expectation1 = XCTestExpectation(
+            description: "Local results received"
+        )
+        let expectation2 = XCTestExpectation(
+            description: "Repository flow completed"
+        )
+        
+        // When
+        let flow = repository
+            .fetch(on: .immediate)
+            .onLocal { results in
+                mockedMemorySource.append(contentsOf: results)
+                expectation1.fulfill()
+            }
+            .onRemote { results, label in
+                remoteCalled = true
+            }
+            .onCompletion {
+                expectation2.fulfill()
+            }
+        
+        flow.cancellable.store(in: &cancellables)
+         wait(for: [expectation1, expectation2], timeout: 1.0)
+        
+        // Then
+        XCTAssertFalse(remoteCalled)
+        XCTAssertEqual(mockedMemorySource.count, mockedLocalSource.count)
+        XCTAssertEqual(mockedMemorySource.first, mockedLocalSource.first)
+        XCTAssertEqual(mockedMemorySource.last, mockedLocalSource.last)
+        XCTAssertTrue(flow.hasCompleted)
+    }
+    
 }
