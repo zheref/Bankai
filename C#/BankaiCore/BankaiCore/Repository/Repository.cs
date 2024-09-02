f1f50b4a65d72de4d87ea3a42dddee7680d4b0b7
Sergio Daniel <me@zheref.io>
2024-09-02 15:17:26 -0500

    Spoils of Repository

---
+++ C#/BankaiCore/BankaiCore/Repository/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankaiCore.Repository;
+
+public interface Filter
+{
+    bool onlyLocally { get; set; }
+}
+
+public interface Identifiable
+{
+    string id { get; set; }
+}
+
+public interface Traceable: Identifiable
+{
+    DateTime updatedAt { get; set; }
+}
+
+public interface DataSource<T, F>
+{
+    Task prepare();
+}
+
+public interface LocalDataSource<T, F>: DataSource<T, F>
+{
+    Task store(IEnumerable<T> items);
+    Task<IEnumerable<T>> retrieve(F? filter);
+}
+
+public interface RemoteDataSource<T, F> : DataSource<T, F>
+{
+    static string name { get; }
+    Task<IEnumerable<T>> pull(F? filter);
+    Task push(IEnumerable<T> items);
+}
+
+public interface Repository<T, F>: DataSource<T, F>
+{
+    LocalDataSource<T, F> local { get; set; }
+    List<RemoteDataSource<T, F>> remotes { get; set; }
+
+    Task save(IEnumerable<T> items, bool shouldAttemptToPush);
+}
