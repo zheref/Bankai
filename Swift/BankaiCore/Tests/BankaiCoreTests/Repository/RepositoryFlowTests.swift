4886bdef1f2707210188aef96dbb13e47eaf70c5
Sergio Daniel <me@zheref.io>
2024-09-04 00:43:43 -0500

    Add regular use of repository flow

--- Swift/BankaiCore/Tests/BankaiCoreTests/Repository/RepositoryFlowTests.swift
+++ Swift/BankaiCore/Tests/BankaiCoreTests/Repository/RepositoryFlowTests.swift
@@ -54,4 +54,48 @@ struct RepositoryFlowTests {
         cancellable.cancel()
     }
     
+    @MainActor
+    @Test
+    func testRegularRepositoryFlow() async {
+        let testScheduler: AnySchedulerOf<DispatchQueue> = .immediate
+        var lastNumber = 0
+        var remoteReceived: String?
+        var hasCompleted = false
+        var cancellable: AnyCancellable!
+        
+        // Given
+        let sut = RepositoryFlow<Int>(onlyLocalExpected: false,
+                                      scheduler: testScheduler) { receiver in
+            try await Task.sleep(seconds: 0.5)
+            receiver.send(local: 1)
+            
+            try await Task.sleep(seconds: 0.5)
+            receiver.send(remote: 2, from: "api")
+        }
+        
+        // When
+        await confirmation { confirmation in
+            sut.onLocal {
+                lastNumber = $0
+            }.onRemote { data, label in
+                lastNumber = data
+                remoteReceived = label
+            }.onCompletion {
+                hasCompleted = true
+                confirmation()
+            }
+            
+            cancellable = sut.run()
+            try! await Task.sleep(seconds: 2)
+        }
+        
+        // Then
+        #expect(lastNumber == 2)
+        #expect(remoteReceived == "api")
+        #expect(hasCompleted)
+        #expect(sut.hasCompleted)
+        
+        cancellable.cancel()
+    }
+    
 }
