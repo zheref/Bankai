cf6930355ff56021b1b22c6ace4ac5c07d948cd4
Sergio Daniel <me@zheref.io>
2024-09-15 01:19:27 -0500

    Introduce StoreIn for obsevables (publishers/flows) on C#

---
+++ C#/BankaiCore/BankaiCore/Reactive/ReactiveExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankaiCore.Reactive;
+
+public static class ReactiveExtensions
+{
+    /// <summary>
+    /// Adds the disposable to the target set.
+    /// </summary>
+    /// <param name="self">The set to be added</param>
+    /// <param name="targetSet">The set where to add the disposable</param>
+    public static void StoreIn(
+        this IDisposable self, 
+        ref HashSet<IDisposable> targetSet
+    ) => targetSet.Add(self);
+}
