1c4c7d47d771c7bfff1cb114b2215612e0bdbef6
zheref <me@zheref.io>
2024-09-26 23:24:57 -0500

    Fix macOS compatibility

--- Swift/BankaiApple/Sources/BankaiApple/SwiftUI-MiniComponents.swift
+++ Swift/BankaiApple/Sources/BankaiApple/SwiftUI-MiniComponents.swift
@@ -15,14 +15,26 @@ public var isWide = false
 #endif
 
 #if os(iOS) || os(tvOS) || targetEnvironment(macCatalyst)
+import UIKit
+#endif
 
+#if os(macOS)
+import AppKit
 #endif
 
 @MainActor
 @ViewBuilder
 public func ButtonLabel(glyph: String, text: String, short: String? = nil) -> some View {
     HStack {
-        Image(systemName: glyph)
+        if #available(macOS 11.0, *) {
+            Image(systemName: glyph)
+        } else {
+            #if os(macOS)
+            EmptyView()
+            #else
+            Image(uiImage: UIImage(systemName: glyph))
+            #endif
+        }
         if isWide { Text(text) }
         else if let short { Text(short) }
     }
