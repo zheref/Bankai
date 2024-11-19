ba085cb973bb3ce9ed329125f63e335b81d84d19
Sergio Daniel <me@zheref.io>
2024-11-18 20:40:01 -0500

    Progress on Kro model for C#

--- C#/BankaiCore/BankaiCore/Common/DateExtensions.cs
+++ C#/BankaiCore/BankaiCore/Common/DateExtensions.cs
@@ -9,4 +9,17 @@ namespace BankaiCore.Common;
 public static class DateExtensions
 {
     public static DateTime oneDayOut(this DateTime self) => self.AddDays(1);
+
+    /// <summary>
+    /// Calculates the difference between the given date and this date
+    /// and returns it as a TimeSpan
+    /// </summary>
+    /// <param name="self">The reference to this date to use as starting point</param>
+    /// <param name="date">The date to be used as ending point to calculate time span</param>
+    /// <returns>The </returns>
+    public static TimeSpan durationSince(this DateTime self, DateTime date)
+        => date.Subtract(self);
+
+    public static String digitalDuration(this DateTime self, bool includingSeconds = false)
+        => self.TimeOfDay.digitalDuration(includingSeconds);
 }
---
+++ C#/BankaiCore/BankaiCore/Common/DurationExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace BankaiCore.Common;
+
+public static class DurationExtensions
+{
+    /// <summary>
+    /// This integer interpreted as a TimeSpan in hours.
+    /// </summary>
+    /// <param name="self">This integer number</param>
+    /// <returns>The corresponding <see cref="TimeSpan"/> interpretation in hours</returns>
+    public static TimeSpan hours(this int self)
+        => TimeSpan.FromHours(self);
+
+
+    /// <summary>
+    /// This integer interpreted as a TimeSpan in minutes.
+    /// </summary>
+    /// <param name="self">This integer number</param>
+    /// <returns>The corresponding <see cref="TimeSpan"/> interpretation in minutes</returns>
+    public static TimeSpan minutes(this int self)
+        => TimeSpan.FromMinutes(self);
+
+    /// <summary>
+    /// This integer interpreted as a TimeSpan in seconds.
+    /// </summary>
+    /// <param name="self">This integer number</param>
+    /// <returns>The corresponding <see cref="TimeSpan"/> interpretation in seconds</returns>
+    public static TimeSpan seconds(this int self)
+        => TimeSpan.FromSeconds(self);
+
+    /// <summary>
+    /// A string representation of this TimeSpan in digital format.
+    /// As in: "00:00:00"
+    /// </summary>
+    /// <param name="self">This duration as a TimeSpan</param>
+    /// <param name="includingSeconds">Whether to include seconds in the output</param>
+    /// <returns>Digital formatted string reading this duration</returns>
+    public static string digitalDuration(this TimeSpan self, bool includingSeconds = false)
+        => includingSeconds ? 
+              $"{self.Hours:D2}:{self.Minutes:D2}:{self.Seconds:D2}"
+            : $"{self.Hours:D2}:{self.Minutes:D2}";
+}
