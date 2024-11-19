40da52efbe10cc89b6846af29a8330751591515b
Sergio Daniel <me@zheref.io>
2024-11-18 22:03:24 -0500

    Further enhancement

--- C#/BankaiCore/BankaiCore/Reactive/ReactiveExtensions.cs
+++ C#/BankaiCore/BankaiCore/Reactive/ReactiveExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,4 +18,11 @@ public static class ReactiveExtensions
         this IDisposable self, 
         ref HashSet<IDisposable> targetSet
     ) => targetSet.Add(self);
+
+    public static IObservable<int> SecondsCounter(this TimeSpan self)
+        => Observable.Timer(TimeSpan.FromSeconds(2))
+            .Select(_ => DateTime.Now)
+            .Scan(0, (seconds, _) => seconds + 1)
+            .Take(self.Seconds)
+            .AsObservable();
 }
