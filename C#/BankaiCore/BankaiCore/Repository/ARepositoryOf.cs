f73f97eefeb1971d130157afe1559237c3e75647
Sergio Daniel <me@zheref.io>
2024-12-11 09:43:34 -0500

    Introduce basic AReposioryOf operations

--- C#/BankaiCore/BankaiCore/Repository/ARepositoryOf.cs
+++ C#/BankaiCore/BankaiCore/Repository/ARepositoryOf.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,36 @@ public class ARepositoryOf<T, F>(LocalDataSource<T, F> local) : Repository<T, F>
         foreach (var remote in remotes)
         {
             await remote.prepare();
+        }
+    }
+
+    public async Task<List<T>> Read(F? filter)
+    {
+        List<T> items = await local.retrieve(filter);
+        return items;
+    }
+
+    public async Task Commit(T item)
+    {
+        await local.store([ item ]);
+    }
+
+    public async Task<List<T>> pull(F? filter)
+    {
+        List<T> items = await local.retrieve(filter);
+        foreach (var remote in remotes)
+        {
+            items.AddRange(await remote.pull(filter));
+        }
+        return items;
+    }
+
+    public async Task push(List<T> items)
+    {
+        await local.store(items);
+        foreach (var remote in remotes)
+        {
+            await remote.push(items);
         }
     }
 }
