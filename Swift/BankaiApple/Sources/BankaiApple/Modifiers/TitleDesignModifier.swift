7f3e400cb11b6882c5f00b9b796d0091fa13880a
Sergio Daniel <me@zheref.io>
2025-02-18 22:40:51 -0500

    Title design modifier

--- Swift/BankaiApple/Sources/BankaiApple/Modifiers/TitleDesignModifier.swift
+++ Swift/BankaiApple/Sources/BankaiApple/Modifiers/TitleDesignModifier.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TitleDesignModifier: ViewModifier {
+    #if os(macOS)
     func body(content: Content) -> some View {
         if #available(macOS 13.0, *) {
             content
@@ -20,6 +21,24 @@ struct TitleDesignModifier: ViewModifier {
                 .font(.system(size: 30, weight: .bold, design: .rounded))
         }
     }
+    #else
+    func body(content: Content) -> some View {
+        if #available(iOS 16.0, *) {
+            content
+                .font(.largeTitle)
+                .bold()
+        } else if #available(iOS 16.1, *) {
+            content
+                .font(.largeTitle)
+                .bold()
+                .fontDesign(.rounded)
+        } else {
+            content
+                .font(.largeTitle)
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+        }
+    }
+    #endif
 }
 
 extension View {
